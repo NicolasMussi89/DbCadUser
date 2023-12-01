a881c7b1ae3ca4016429a7f6128cffc42678d862
Nicolas <nmussidasilva@gmail.com>
2023-12-01 11:14:44 -0300

    add usuario php

--- cadastrar.php
+++ cadastrar.php
@@ -1,4 +1,7 @@
 <?php 
+
+include('conexao.php')
+
 /*variaveis para armazenamento do form */
 $nome = $_POST['nome'];
 $nome = $_POST['gmail'];
@@ -8,6 +11,16 @@ $nome = $_POST['senha'];
 echo "<h3>Nome: $nome</h3>";
 echo "<h3>Nome: $gmail</h3>";
 echo "<h3>senha: A senha é secreta</h3>": 
+
 $cad_usuario = "INSERT INTO usuario(nome,email,senha) VALUES"
-"('$nome', $'email', 'senha')";
+"('$nome', $'email', 'senha')"
+
+//teste de condição para rodar SQL
+if(mysqli_query($conn, $cad_usuario)){
+    echo "<h1>Novo cadastro realidade</h1>";
+}else{
+    each "ERRO: " . $cad_usuario . "</br>" . mysqli_error($conn)
+}
+//mysqli_close($conn);
+
 ?>
